b2e140a9c4c699192e4fe88718e4af1e3be4a92e
flyingyizi <flyingyizi@gmail.com>
2020-05-04 22:38:10 +0800

    f

--- app/src/main/kotlin/com/example/app/AppDemo.kt
+++ app/src/main/kotlin/com/example/app/AppDemo.kt
@@ -1,5 +1,8 @@
 package com.example.app
 
+//见[Calling Kotlin from JavaScript](https://kotlinlang.org/docs/reference/js-to-kotlin-interop.html)
+//说明，需要在js中调用的kotlin代码使用@JsName声明下，从实践看，如果不采取该措施，有时候会有问题，即名称不一定
+
 @JsName("sayHello")
 fun sayHello() {
     com.example.base.sayHello("新手")
